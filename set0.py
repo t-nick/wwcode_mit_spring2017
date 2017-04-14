f9506013479d063dcbbfcaae34e588f6cf136a61
Tanya <nickolaychuk.t@gmail.com>
2017-04-14 17:18:37 +0300

    first commit - first project

---
+++ set0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  8 23:29:58 2017
+
+@author: Tanichka
+"""
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+z = x**y
+print(z)
+import math
+p = int(math.log(x, 2))
+print(p)
